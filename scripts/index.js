940e38abe865f46e5c9f1c0945aae9e5a1f7cdbc
Amio <amio.cn@gmail.com>
2014-05-06 10:34:16 +0800

    添加顶部导航随滚动滑出的JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,19 @@
+$(function () {
 
-$(function(){
-
-  $("#ws-tab").bind("click", "a", function(e){
-    var tab = $(e.target).attr("class").replace("tab-","");
+  $("#ws-tab").bind("click", "a", function (e) {
+    var tab = $(e.target).attr("class").replace("tab-", "");
     $(".schedule-ws").removeClass("fe ix ur vd").addClass(tab);
   });
-  
-  $(window).on('scroll',function(e){
-    console.log(e);
+
+  var bannerHeight = $('.banner').height();
+  var header = $('header');
+  $(window).on('scroll', function () {
+    if (document.body.scrollTop > bannerHeight) {
+      header.addClass('shown');
+    } else {
+      header.removeClass('shown');
+    }
   });
+
 });
 
